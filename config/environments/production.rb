aaddb4c900a7f0eb9083943ddffc6bbb19f161f0
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-12-29 23:22:36 -0900

    what?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,16 +21,16 @@ Rachelsblog::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rachelsblog::Application.routes.draw do
   resources :users
   resources :posts
+  get 'about', to: 'home#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
